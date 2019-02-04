86fbb2aeec4833cbcd1d21f8ff71efb0ed3c9042
Servio Palacios <servio@palacios.com>
2019-02-04 15:03:22 -0500

    Updates

--- precache-manifest.2a142bbaade8a7ea86e02a092a34111a.js
+++ precache-manifest.2a142bbaade8a7ea86e02a092a34111a.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/FieldWorkApp/static/js/runtime~main.af89c2e9.js"
   },
   {
-    "revision": "b2cf33255b24f1727e0b",
-    "url": "/FieldWorkApp/static/js/main.b2cf3325.chunk.js"
+    "revision": "7f418351457327ee7c4e",
+    "url": "/FieldWorkApp/static/js/main.7f418351.chunk.js"
   },
   {
     "revision": "f719f604b7ea3ef8616e",
     "url": "/FieldWorkApp/static/js/1.f719f604.chunk.js"
   },
   {
-    "revision": "b2cf33255b24f1727e0b",
+    "revision": "7f418351457327ee7c4e",
     "url": "/FieldWorkApp/static/css/main.8782abe4.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/FieldWorkApp/static/css/1.0f2def8d.chunk.css"
   },
   {
-    "revision": "87555558d20461b7f1d03e874928a750",
+    "revision": "327ca2987227aabbde1bae43f54a2ca3",
     "url": "/FieldWorkApp/index.html"
   }
 ];
