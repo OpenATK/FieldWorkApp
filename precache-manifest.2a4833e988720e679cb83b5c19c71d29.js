8b2d819fffc683a42bdffa4335f0b3209c73a6d8
Servio Palacios <servio@palacios.com>
2019-02-20 11:44:49 -0500

    Updates

---
+++ precache-manifest.2a4833e988720e679cb83b5c19c71d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af89c2e95c86dfc31cb1",
+    "url": "/FieldWorkApp/static/js/runtime~main.af89c2e9.js"
+  },
+  {
+    "revision": "ab0230432d5ca6dee64c",
+    "url": "/FieldWorkApp/static/js/main.ab023043.chunk.js"
+  },
+  {
+    "revision": "b318f3814563ac359c69",
+    "url": "/FieldWorkApp/static/js/1.b318f381.chunk.js"
+  },
+  {
+    "revision": "ab0230432d5ca6dee64c",
+    "url": "/FieldWorkApp/static/css/main.4bc37f95.chunk.css"
+  },
+  {
+    "revision": "b318f3814563ac359c69",
+    "url": "/FieldWorkApp/static/css/1.ec133a22.chunk.css"
+  },
+  {
+    "revision": "063b99ad08b7fa4e140000e25e7e329b",
+    "url": "/FieldWorkApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/FieldWorkApp/precache-manifest.aa76e2c758b884f2da203fcdaea246fa.js"
+  "/FieldWorkApp/precache-manifest.2a4833e988720e679cb83b5c19c71d29.js"
 );
 
 workbox.clientsClaim();
