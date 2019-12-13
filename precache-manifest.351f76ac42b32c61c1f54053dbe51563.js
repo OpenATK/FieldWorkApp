428951cb6228ae20540ffb25ffb85742e4b3f3fe
Cyrus Bowman <cyrusbow@gmail.com>
2019-12-13 15:49:00 -0500

    Updates

---
+++ precache-manifest.351f76ac42b32c61c1f54053dbe51563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797e8906ceeb6aa7b3dcfd14036ca54a",
+    "url": "/FieldWorkApp/index.html"
+  },
+  {
+    "revision": "b8038eee94febef62aa1",
+    "url": "/FieldWorkApp/static/css/main.b19c127a.chunk.css"
+  },
+  {
+    "revision": "967fddfb1400791db353",
+    "url": "/FieldWorkApp/static/js/2.40de7afd.chunk.js"
+  },
+  {
+    "revision": "b8038eee94febef62aa1",
+    "url": "/FieldWorkApp/static/js/main.8e678479.chunk.js"
+  },
+  {
+    "revision": "ac9c6140da60125500db",
+    "url": "/FieldWorkApp/static/js/runtime~main.c3fd910e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FieldWorkApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21ad9c587bc3374196c48e2035e0f499.js"
+  "/FieldWorkApp/precache-manifest.351f76ac42b32c61c1f54053dbe51563.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FieldWorkApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
