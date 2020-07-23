a6d799cab34e08e0cd54ed13f0405ae93ba672ee
Cyrus Bowman <cyrusbow@gmail.com>
2020-07-23 14:51:45 -0400

    Updates

---
+++ precache-manifest.10a7522c6e612cb7cebaf6eccd5f13d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104b89a79b4946a85245046bcc7b4458",
+    "url": "/FieldWorkApp/index.html"
+  },
+  {
+    "revision": "384c4f6257545cf08e7b",
+    "url": "/FieldWorkApp/static/css/main.b19c127a.chunk.css"
+  },
+  {
+    "revision": "5a556babef2c9dcbd4e9",
+    "url": "/FieldWorkApp/static/js/2.316c7812.chunk.js"
+  },
+  {
+    "revision": "fc7188b1ec82b38cc24f42e2c1a03eed",
+    "url": "/FieldWorkApp/static/js/2.316c7812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384c4f6257545cf08e7b",
+    "url": "/FieldWorkApp/static/js/main.6750dd9e.chunk.js"
+  },
+  {
+    "revision": "61b82375f177964d1478",
+    "url": "/FieldWorkApp/static/js/runtime-main.d917edd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FieldWorkApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
