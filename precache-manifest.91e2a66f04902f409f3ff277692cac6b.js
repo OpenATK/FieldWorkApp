d6255536e07065dc1068e1e7ca045080f0cf6ab2
Cyrus Bowman <cyrusbow@gmail.com>
2020-09-08 15:37:46 -0400

    Updates

---
+++ precache-manifest.91e2a66f04902f409f3ff277692cac6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fffcb0eea98bfc4648d4ae5f384ebe",
+    "url": "/FieldWorkApp/index.html"
+  },
+  {
+    "revision": "bf49b7f3476ac6b58b2a",
+    "url": "/FieldWorkApp/static/css/main.b19c127a.chunk.css"
+  },
+  {
+    "revision": "0952f41e0d4276b16f0d",
+    "url": "/FieldWorkApp/static/js/2.a5c5fba5.chunk.js"
+  },
+  {
+    "revision": "fc7188b1ec82b38cc24f42e2c1a03eed",
+    "url": "/FieldWorkApp/static/js/2.a5c5fba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf49b7f3476ac6b58b2a",
+    "url": "/FieldWorkApp/static/js/main.d125cfef.chunk.js"
+  },
+  {
+    "revision": "61b82375f177964d1478",
+    "url": "/FieldWorkApp/static/js/runtime-main.d917edd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FieldWorkApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FieldWorkApp/precache-manifest.00989ff7c8647b29874ec02c0b23eba5.js"
+  "/FieldWorkApp/precache-manifest.91e2a66f04902f409f3ff277692cac6b.js"
 );
 
 self.addEventListener('message', (event) => {
